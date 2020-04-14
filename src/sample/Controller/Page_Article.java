000bbb788eebdcdb06efcd58c393b0773e8a7a3c
Dewi Chantika Maya <dewi.chantikamaya06@gmail.com>
2020-04-14 20:28:43 +0700

    curhat dan profile

---
+++ src/sample/Controller/Page_Article.java
@@ -0,0 +1,68 @@
+package sample.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+
+public class Page_Article {
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private Button btnPost;
+
+    @FXML
+    private Button btnPsikolog;
+
+    @FXML
+    private Button btnProfile;
+
+    @FXML
+    private TextField btnSearching;
+
+    @FXML
+    private Button btnSetProfile;
+
+    @FXML
+    void clickBtnHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnPost(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnPsikolog(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnSetProfile(ActionEvent event) {
+
+    }
+
+    public void clickBtnHome(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnPost(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnPsikolog(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnProfile(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnSetProfile(javafx.event.ActionEvent event) {
+    }
+}
