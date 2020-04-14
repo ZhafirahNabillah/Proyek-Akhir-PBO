f3c82dc8965de3fc3b2a72667d7bf57db79a3db4
ZhafirahNabillah <55244721+ZhafirahNabillah@users.noreply.github.com>
2020-04-14 17:19:58 +0700

    hehe

--- src/sample/Controller/Page_Post.java
+++ src/sample/Controller/Page_Post.java
@@ -1,4 +1,26 @@
 package sample.Controller;
 
+import javafx.event.ActionEvent;
+
 public class Page_Post {
+    public void clickBtnHome(ActionEvent actionEvent) {
+    }
+
+    public void clickBtnPost(ActionEvent actionEvent) {
+    }
+
+    public void clickBtnPsikolog(ActionEvent actionEvent) {
+    }
+
+    public void clickBtnProfile(ActionEvent actionEvent) {
+    }
+
+    public void clickBtnLikes(ActionEvent actionEvent) {
+    }
+
+    public void clickBtnComment(ActionEvent actionEvent) {
+    }
+
+    public void clickBtnPostingan(ActionEvent actionEvent) {
+    }
 }
