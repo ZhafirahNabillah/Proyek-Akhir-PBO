1144de4e091a585f7c8dd78899100378dc092737
Dewi Chantika Maya <dewi.chantikamaya06@gmail.com>
2020-04-14 19:02:59 +0700

    curhat dan profile

--- src/sample/Controller/Set_Profile.java
+++ src/sample/Controller/Set_Profile.java
@@ -1,4 +1,137 @@
 package sample.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+import java.awt.event.ActionEvent;
+
 public class Set_Profile {
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private Button btnPost;
+
+    @FXML
+    private Button btnPsikolog;
+
+    @FXML
+    private Button btnProfile;
+
+    @FXML
+    private Text txt_Name;
+
+    @FXML
+    private Text txt_Username;
+
+    @FXML
+    private Button btnName;
+
+    @FXML
+    private Button btnStatus;
+
+    @FXML
+    private Button btnUsername;
+
+    @FXML
+    private Button btnEmail;
+
+    @FXML
+    private Button btnPassword;
+
+    @FXML
+    private Button btnSetProfile;
+
+    @FXML
+    private Button btnChangephoto;
+
+    @FXML
+    void clickBtnChangephoto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnEmail(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnName(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnPassword(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnPost(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnPsikolog(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnSetProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnStatus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnUsername(ActionEvent event) {
+
+    }
+
+    public void clickBtnHome(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnPost(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnPsikolog(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnProfile(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnName(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnStatus(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnUsername(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnEmail(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnPassword(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnSetProfile(javafx.event.ActionEvent event) {
+    }
+
+    public void clickBtnChangephoto(javafx.event.ActionEvent event) {
+    }
 }
