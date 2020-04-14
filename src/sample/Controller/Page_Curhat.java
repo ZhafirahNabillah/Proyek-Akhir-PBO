aca8a9d04fec34733d5eb66727f5d2e569718e56
Dewi Chantika Maya <dewi.chantikamaya06@gmail.com>
2020-04-14 13:31:49 +0700

    push

---
+++ src/sample/Controller/Page_Curhat.java
@@ -0,0 +1,74 @@
+package sample.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+
+public class Page_Curhat {
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private Button btnPost;
+
+    @FXML
+    private Button btnPsikolog;
+
+    @FXML
+    private Button btnProfile;
+
+    @FXML
+    private Button btnLikes;
+
+    @FXML
+    private Button btnComment;
+
+    @FXML
+    private Button btnSend;
+
+    @FXML
+    private Button btnReport;
+
+    @FXML
+    void clickBtnComment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnLikes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnPost(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnPsikolog(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnReport(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnSend(ActionEvent event) {
+
+    }
+
+}
