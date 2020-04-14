cc49e1b9e35b0ba25753c1b6633caad846eb60a9
ZhafirahNabillah <55244721+ZhafirahNabillah@users.noreply.github.com>
2020-04-14 19:32:01 +0700

    hehe

--- src/sample/Controller/Page_Post.java
+++ src/sample/Controller/Page_Post.java
@@ -1,7 +1,11 @@
 package sample.Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
 
