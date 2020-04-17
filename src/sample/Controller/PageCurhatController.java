e33ba2312665a1caf932455e0adebbfa303a7ac8
Dewi Chantika Maya <dewi.chantikamaya06@gmail.com>
2020-04-17 18:37:24 +0700

    admin

--- src/sample/Controller/PageCurhatController.java
+++ src/sample/Controller/PageCurhatController.java
@@ -1,13 +1,8 @@
 package sample.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
-
-import java.awt.event.ActionEvent;
 
 public class PageCurhatController {
 
@@ -24,18 +19,7 @@ public class PageCurhatController {
     private Button btnProfile;
 
     @FXML
-    private Button btnForm;
-
-    @FXML
     private Button btnSend;
-
-    @FXML
-    private Button btnReport;
-
-    @FXML
-    void clickBtnForm(ActionEvent event) {
-
-    }
 
     @FXML
     void clickBtnHome(ActionEvent event) {
@@ -62,82 +46,4 @@ public class PageCurhatController {
 
     }
 
-    public void clickBtnHome(javafx.event.ActionEvent event) {
-        ShowDashboard();
-    }
-
-    public void clickBtnPost(javafx.event.ActionEvent event) {
-        ShowPost();
-    }
-
-    public void clickBtnPsikolog(javafx.event.ActionEvent event) {
-        ShowChat();
-    }
-
-    public void clickBtnProfile(javafx.event.ActionEvent event) {
-        ShowProfile();
-    }
-
-    public void clickBtnForm(javafx.event.ActionEvent event) {
-    }
-
-    public void clickBtnSend(javafx.event.ActionEvent event) {
-    }
-
-    void ShowProfile(){
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Views/page_Profile.fxml"));
-            Parent root = loader.load();
-
-            PageProfileController page_profile = loader.getController();
-
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.show();
-        }catch (Exception e){
-            System.err.println(e);
-        }
-    }
-    void ShowPost(){
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Views/page_Post.fxml"));
-            Parent root = loader.load();
-
-            PagePostController page_post= loader.getController();
-
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.show();
-        }catch (Exception e){
-            System.err.println(e);
-        }
-    }
-    void ShowChat(){
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Views/page_Curhat.fxml"));
-            Parent root = loader.load();
-
-            PageCurhatController page_curhat = loader.getController();
-
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.show();
-        }catch (Exception e){
-            System.err.println(e);
-        }
-    }
-    void ShowDashboard(){
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Views/Dashboard.fxml"));
-            Parent root = loader.load();
-
-            DashboardController dashboard = loader.getController();
-
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.show();
-        }catch (Exception e){
-            System.err.println(e);
-        }
-    }
 }
