05f6fd1775101757b3097378c296074469cccf88
Dewi Chantika Maya <dewi.chantikamaya06@gmail.com>
2020-04-18 00:10:16 +0700

    admin

--- src/sample/Controller/PsikologReportController.java
+++ src/sample/Controller/PsikologReportController.java
@@ -1,4 +1,49 @@
 package sample.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class PsikologReportController {
+
+    @FXML
+    private Button btnPsikolog;
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    private Button btnDetail;
+
+    @FXML
+    private Button btnSee;
+
+    @FXML
+    void clickBtnHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnPsikolog(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickDetail(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickLogout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSee(ActionEvent event) {
+
+    }
+
 }
