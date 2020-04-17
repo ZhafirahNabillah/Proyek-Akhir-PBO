0098eafae98f040bf0803e4334d410177609c827
Dewi Chantika Maya <dewi.chantikamaya06@gmail.com>
2020-04-17 23:54:17 +0700

    admin

---
+++ src/sample/Controller/PsikologProfileController.java
@@ -0,0 +1,26 @@
+package sample.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class PsikologProfileController {
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private Button btnProfile;
+
+    @FXML
+    void clickBtnHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnProfile(ActionEvent event) {
+
+    }
+
+}
