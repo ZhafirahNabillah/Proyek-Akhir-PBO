a5600a1aa827e20d4872e3b894ea652ec11c6430
Dewi Chantika Maya <dewi.chantikamaya06@gmail.com>
2020-04-18 00:18:12 +0700

    admin

--- src/sample/Controller/PsikologSaranController.java
+++ src/sample/Controller/PsikologSaranController.java
@@ -4,7 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class PsikologReportController {
+public class PsikologSaranController {
 
     @FXML
     private Button btnPsikolog;
