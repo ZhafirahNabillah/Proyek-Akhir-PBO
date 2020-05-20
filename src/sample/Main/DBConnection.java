616b7df16603016d22e3e55a9b0536105959d991
Dewi Chantika Maya <dewi.chantikamaya06@gmail.com>
2020-05-20 12:26:51 +0700

    DBConnection

--- src/sample/Main/DBConnection.java
+++ src/sample/Main/DBConnection.java
@@ -5,24 +5,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    Connection connection = null;
-    public static final String DBMS = "mysql";
-    public static final String HOST = "128.199.196.208";
-    public static final String USERNAME = "PuffGirls";
-    public static final String PASSWORD = "proyekakhirpbo2";
-    public static final String DATABASE = "PowerPuffGirls";
+    private static Connection con = null;
+//    public static final String DBMS = "mysql";
+//    public static final String HOST = "128.199.196.208";
+//    public static final String USERNAME = "PuffGirls";
+//    public static final String PASSWORD = "proyekakhirpbo2";
+//    public static final String DATABASE = "PowerPuffGirls";
 
-    public static Connection connectDB(){
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            String url = String.format("jdbc:%s://%s/%s", DBMS, HOST, DATABASE);
-            Connection conn = DriverManager.getConnection(url, USERNAME, PASSWORD);
-            return conn;
-        } catch (ClassNotFoundException e) {
-            return null;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
+    public static Connection connect() {
+        if (con == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/powerpuffgirls","root","root")
+                return con;
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
